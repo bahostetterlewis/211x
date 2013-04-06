e51e92e01c8eae91249540bd70b8bf29b655f861
Barrett <musikal.fusion@gmail.com>
2013-04-05 17:16:40 -0700

    Update output2.cpp

--- 2013Spring/output-redirection/output2.cpp
+++ 2013Spring/output-redirection/output2.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 void print(ostream *out, string s)
 {
-  *out << s << endl;
+  *out << s << endl; // since we only have a pointer to an output stream we must dereference it before we write
 }
 
 int main(int argc, char* argv[])
@@ -21,15 +21,32 @@ int main(int argc, char* argv[])
   }
   else
   {
+   //outother is a convienence variable, it keeps us from needing to cash
     outother.open(argv[1]); 
     out = &outother;
+    /* 
+    THIS IS THE ALTERNATIVE WITHOUT outother IF YOU USE THIS DELETE THE LINE 'fstream outother;
+    ALSO DELETE ANY LINE MENTIONING outother
+    
+    out = new ofstream(argv[1]);
+    */
   }
 
   print(out, "s");
 
   if(strcmp("cout", argv[1]) != 0)
   {
-    outother.close();
+    (ofstream*)outother.close();
+    
+    /* 
+    THIS IS THE ALTERNATIVE WITHOUT outother IF YOU USE THIS DELETE THE LINE 'fstream outother;
+    ALSO DELETE ANY LINE MENTIONING outother
+    
+    //we cast here because we want to tell the pointer it can find a "close" function
+    //c++ style casts have a higher prcedence than -> so we don't need to wrap it
+    //c style casts would require wrapping the cast in parens before the -> can be applied
+    static_cast<ofstream*>(out)->close();
+    */
   }
 
   return 0;
